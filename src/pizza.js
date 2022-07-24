4cea410f7d02ba58f886cdd3a0da59e8d45274bc
Anthony Gladden <a.gladden360@outlook.com>
2022-07-23 20:38:03 -0700

    created setPrice and setToppingPrice function

--- src/pizza.js
+++ src/pizza.js
@@ -18,3 +18,16 @@ Pizza.prototype.setPrice = function() {
     this.price += 13
   }
 }
+
+Pizza.prototype.setToppingPrice = function() {
+
+  if (this.topping === 'blank'){
+    this.price
+  } else if (this.topping === 'cheese'){
+    this.price = this.price * 3
+  } else if (this.topping === 'pepperoni'){
+    this.price = this.price * 4
+  } else {
+    this.price = this.price * 2
+  }
+};
